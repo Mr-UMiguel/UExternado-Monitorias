36303d8e262c5f84831cffff7428a89e963ab2e4
Miguel <migurlacade99@gmail.com>
2022-08-05 18:06:53 -0500

    Monitoria 2 primera parte

---
+++ Monitoria2/normalize.py
@@ -0,0 +1,8 @@
+def normalize(arr, t_min, t_max):
+    norm_arr = []
+    diff = t_max - t_min
+    diff_arr = max(arr) - min(arr)    
+    for i in arr:
+        temp = (((i - min(arr))*diff)/diff_arr) + t_min
+        norm_arr.append(temp)
+    return norm_arr
